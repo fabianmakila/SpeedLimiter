e9fb436fd607bec04ed9a4334e02d9bc9808a363
Fabian Mäkilä <44451855+FaberoM@users.noreply.github.com>
2021-06-06 14:58:00 +0300

    Check if config exists in the folder before trying saving

--- src/main/java/fi/fabianadrian/speedlimiter/ConfigManager.java
+++ src/main/java/fi/fabianadrian/speedlimiter/ConfigManager.java
@@ -5,10 +5,12 @@ import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.ConfigurationNode;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+//TODO Recode this someday, does the job for now...
 public class ConfigManager {
 
     private final SpeedLimiter plugin;
@@ -35,10 +37,12 @@ public class ConfigManager {
     }
 
     public void load() throws ConfigurateException {
-        plugin.saveResource("config.yml", false);
+        if (!new File(plugin.getDataFolder(), "config.yml").exists())
+            plugin.saveResource("config.yml", false);
         mainConfig = mainConfigLoader.load();
 
-        plugin.saveResource("messages.yml", false);
+        if (!new File(plugin.getDataFolder(), "messages.yml").exists())
+            plugin.saveResource("messages.yml", false);
         messagesConfig = messagesConfigLoader.load();
 
         List<String> stringList = mainConfig.node("disabled-worlds").getList(String.class);
