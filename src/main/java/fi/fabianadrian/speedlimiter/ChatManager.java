2d35d86cbdd6128546ae501e0e708f0133a4e9c3
Fabian Mäkilä <44451855+FaberoM@users.noreply.github.com>
2021-09-06 22:53:55 +0300

    Remove unused import

--- src/main/java/fi/fabianadrian/speedlimiter/ChatManager.java
+++ src/main/java/fi/fabianadrian/speedlimiter/ChatManager.java
@@ -4,7 +4,6 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
