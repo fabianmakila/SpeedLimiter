d0141b627bf77620d49ceee590874bdcd38d2425
Fabian Mäkilä <44451855+FaberoM@users.noreply.github.com>
2021-10-24 20:56:04 +0300

    Update to Gradle 7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import net.kyori.indra.repository.sonatypeSnapshots
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
     id("net.kyori.indra") version "2.0.6"
     id("net.minecrell.plugin-yml.bukkit") version "0.4.0"
     java
