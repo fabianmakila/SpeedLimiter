bf9eb62c6b54de801169644496e1123b3a691b83
fabianmakila <contact@fabianadrian.fi>
2021-12-20 18:23:01 +0200

    Fix prefix capitalization

--- src/main/java/fi/fabianadrian/speedlimiter/Constants.java
+++ src/main/java/fi/fabianadrian/speedlimiter/Constants.java
@@ -4,7 +4,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class Constants {
-    public static final Component COMMAND_PREFIX = MiniMessage.get().parse("<white>[<aqua>Speedlimiter</aqua>]</white>");
+    public static final Component COMMAND_PREFIX = MiniMessage.get().parse("<white>[<aqua>SpeedLimiter</aqua>]</white>");
 
     public static final String PERMISSION_ADMIN = "speedlimiter.admin";
     public static final String PERMISSION_BYPASS = "speedlimiter.bypass";
